41a8f5c046d0ab19b17333aedd76e0d3c154b930
Cristian-Stefan Vasile <cristianvasile23@gmail.com>
2023-06-18 21:14:51 +0300

    Improve display of the faction config tab and add localization

--- js/preloadTemplates.js
+++ js/preloadTemplates.js
@@ -1,6 +1,9 @@
 export async function preloadTemplates() {
   const templates = [
     "modules/conversation-hud/templates/fragments/conversation_participants_list.hbs",
+    "modules/conversation-hud/templates/fragments/participant_data_config_tab.hbs",
+    "modules/conversation-hud/templates/fragments/faction_data_config_tab.hbs",
+
     "modules/conversation-hud/templates/banner-shapes/shape-1.hbs",
     "modules/conversation-hud/templates/banner-shapes/shape-2.hbs",
     "modules/conversation-hud/templates/banner-shapes/shape-3.hbs",
