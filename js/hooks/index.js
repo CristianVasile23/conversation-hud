2e4bf881a799a5cddae5b2ec4eda77913fccb9ec
Cristian Vasile <cristianvasile23@gmail.com>
2025-06-28 22:52:40 +0300

    Rework: Update file name

--- js/hooks/index.js
+++ js/hooks/index.js
@@ -2,7 +2,7 @@ import { registerHook as registerChatMessageHook } from "./chatMessage.mjs";
 import { registerHook as registerRenderChatMessageHook } from "./renderChatMessage.mjs";
 import { registerHook as registerRenderLayoutHook } from "./renderLayout.mjs";
 import { registerHook as registerRenderSceneConfigHook } from "./renderSceneConfig.mjs";
-import { registerHook as registerRenderSettingsConfigHook } from "./renderSettingsConfig.js";
+import { registerHook as registerRenderSettingsConfigHook } from "./renderSettingsConfig.mjs";
 import { registerHook as registerRenderTokenConfigHook } from "./renderTokenConfig.js";
 import { registerHook as registerUpdateSceneHook } from "./updateScene.mjs";
 
